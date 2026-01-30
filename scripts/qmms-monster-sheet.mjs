416583ea85a1d4c7c9443c870d076e9ccc5af878
Ferdinand Engländer <ferdinand@creanimo.net>
2026-01-30 23:36:43 +0000

    troubleshoot prose mirror submit

--- scripts/qmms-monster-sheet.mjs
+++ scripts/qmms-monster-sheet.mjs
@@ -169,7 +169,7 @@ export function createQuickMinimalMonsterSheetClass({
 
             const bioEditor = root.querySelector('prose-mirror[name="system.details.biography.value"]');
             if (bioEditor) {
-                bioEditor.addEventListener("blur", () => this.submit({ preventClose: false, preventRender: false }));
+                bioEditor.addEventListener("blur", () => this.submit());
             }
 
             root.querySelector(".qmms__health__bar__fill").style.width = context.qmms.hp.percentage + "%";
