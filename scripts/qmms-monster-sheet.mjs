22804fc4a933d246239695cabbabf63b069aa3aa
Ferdinand Engländer <ferdinand@creanimo.net>
2026-01-30 22:52:09 +0000

    fix form data

--- scripts/qmms-monster-sheet.mjs
+++ scripts/qmms-monster-sheet.mjs
@@ -12,7 +12,7 @@ export function createQuickMinimalMonsterSheetClass({
         const data = formData?.object ?? formData;
 
         const updateData = {
-            "actor.name": foundry.utils.getProperty(actor, "name"),
+            "actor.name": foundry.utils.getProperty(data, "actor.name"),
             "system.attributes.ac.value": foundry.utils.getProperty(data, "system.attributes.ac.value"),
             "system.attributes.hp.value": foundry.utils.getProperty(data, "system.attributes.hp.value"),
             "system.attributes.hp.max": foundry.utils.getProperty(data, "system.attributes.hp.max"),
